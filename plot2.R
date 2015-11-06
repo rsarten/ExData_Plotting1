6f6d37707bab8c6beb2c0f7ea0d90c789a82aaa4
rsarten <rorysarten@gmail.com>
2015-11-06 21:39:15 +1300

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,24 @@
+###########################################################################################
+# R script for producing Plot 2
+# Assumes file 'household_power_consumption.txt' has been extracted
+# from .zip and sits in same directory as script.
+library(dplyr)
+library(tidyr)
+library(lubridate)
+###########################################################################################
+# Load in data and amalgamate date and time data
+df_first <- read.table("./household_power_consumption.txt", sep=";", skip=66637, nrows=2880,
+                       col.names = c("Date", "Time", "Global_active_power",
+                                     "Global_reactive_power", "Voltage",
+                                     "Global_intensity", "Sub_metering_1",
+                                     "Sub_metering_2", "Sub_metering_3"), stringsAsFactors = F)
+
+df <- df_first %>% mutate(date = paste(Date, Time, sep = " ")) %>% 
+  select(date, Global_active_power:Sub_metering_3)
+df$date <- dmy_hms(df$date)
+###########################################################################################
+# Plot 2
+png("plot2.png", width = 480, height = 480, units = "px")
+with(df, plot(Global_active_power ~ date, type='l',
+              ylab="Global Active Power (kilowatts)", xlab=" "))
+dev.off()
